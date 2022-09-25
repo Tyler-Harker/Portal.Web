1265db7026244acab609bd777477a6d39e31df68
Tyler Harker <tyler@techfabric.co>
2022-09-24 18:17:33 -0700

    chore: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TechfabricModule } from '@techfabric/angular';
+import { MSAL_CONFIG_TOKEN } from '@techfabric/msal-ngrx';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +17,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([]),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    TechfabricModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_CONFIG_TOKEN,
+      useValue: {
+        clientId: environment.azureAd.clientId,
+        tenantId: environment.azureAd.tenantId,
+        redirectUri: environment.azureAd.redirectUri
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
